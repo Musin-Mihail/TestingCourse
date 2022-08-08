1351caa72d18b6d260dc61fdf798bd2604e4a63a
Мусин Михаил <musin.mihail@gmail.com>
2022-08-08 19:30:04 +0300

    Создание пароля

---
+++ CalculatorTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        Calculator calculator = new Calculator();
+        [TestMethod]
+        public void TestPlus()
+        {
+            float x = 4;
+            float y = 5;
+            float actual = calculator.Plus(x,y);
+            float expected = 9;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMinus()
+        {
+            float x = 4;
+            float y = 5;
+            float actual = calculator.Minus(x, y);
+            float expected = -1;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMultiply()
+        {
+            float x = 4;
+            float y = 5;
+            float actual = calculator.Multiply(x, y);
+            float expected = 20;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestDivide()
+        {
+            float x = 4;
+            float y = 5;
+            float actual = calculator.Divide(x, y);
+            float expected = 0.8f;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMultiplyArray1Good()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+            List<float> actual = calculator.MultiplyArray(list);
+            List<float> expected = new List<float>() { 3, 4, 9, 8, 15 };
+            for (int i = 0; i < actual.Count; i++)
+            {
+                if (actual[i] != expected[i])
+                {
+                    Console.WriteLine($"Значение {i} не совпадает: " + actual[i] + " - " + expected[i]);
+
+                    return;
+                }
+            }
+            Console.WriteLine("Все значения совпадают");
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestMultiplyArray2Bad9_10()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+            List<float> actual = calculator.MultiplyArray(list);
+            List<float> expected = new List<float>() { 3, 4, 10, 8, 15 };
+            for (int i = 0; i < actual.Count; i++)
+            {
+                if (actual[i] != expected[i])
+                {
+                    Console.WriteLine($"Значение под индексом {i} не совпадают: " + actual[i] + " - " + expected[i]);
+                    Assert.IsTrue(true);
+                    return;
+                }
+            }
+            Console.WriteLine("Все значения совпадают");
+            Assert.IsTrue(false);
+        }
+    }
+}
---
+++ PasswordTest.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course;
+namespace TestProject1
+{
+    [TestClass]
+    public class PasswordTest
+    {
+        Password password = new Password();
+        Options options = new Options();
+
+        [TestMethod]
+        public void Test1Count()
+        {
+            string actual = password.Create(50, options);
+            Console.WriteLine(actual);
+            Assert.AreEqual(50, actual.Length);
+        }
+        [TestMethod]
+        public void Test2rusCapitalLetterGood()
+        {
+
+            options.rusCapitalLetter = true;
+            string actual = password.Create(100, options);
+            Console.WriteLine(actual);
+            foreach (var item in actual)
+            {
+                if (password.rusSmallLetterList.Contains(item) == true ||
+                    password.engCapitalLetterList.Contains(item) == true ||
+                    password.engSmallLetterList.Contains(item) == true ||
+                    password.symbolsList.Contains(item) == true)
+                {
+                    Assert.Fail($"Символа {item} не должно быть в пароле");
+                    return;
+                }
+            }
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Test3symbolsGood()
+        {
+            options.symbols = true;
+            string actual = password.Create(100, options);
+            Console.WriteLine(actual);
+            foreach (var item in actual)
+            {
+                if (password.rusCapitalLetterList.Contains(item) == true ||
+                    password.rusSmallLetterList.Contains(item) == true ||
+                    password.engCapitalLetterList.Contains(item) == true ||
+                    password.engSmallLetterList.Contains(item) == true)
+                {
+                    Assert.Fail($"Символа {item} не должно быть в пароле");
+                    return;
+                }
+            }
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Test4engSmallLetterBad()
+        {
+            options.engSmallLetter = true;
+            string actual = password.Create(100, options);
+            Console.WriteLine(actual);
+            foreach (var item in actual)
+            {
+                if (password.rusCapitalLetterList.Contains(item) == true ||
+                    password.rusSmallLetterList.Contains(item) == true ||
+                    password.engCapitalLetterList.Contains(item) == true ||
+                    password.engSmallLetterList.Contains(item) == true ||
+                    password.symbolsList.Contains(item) == true)
+                {
+                    Console.WriteLine($"Символа {item} не должно быть в пароле");
+                    Assert.IsTrue(true);
+                    return;
+                }
+            }
+            Assert.IsTrue(false);
+        }
+        [TestMethod]
+        public void Test5ALLGood()
+        {
+            options.rusCapitalLetter = true;
+            options.rusSmallLetter = true;
+            options.engCapitalLetter = true;
+            options.engSmallLetter = true;
+            options.symbols = true;
+
+            bool rusCapitalLetterbool = false;
+            bool rusSmallLetterbool = false;
+            bool engCapitalLetterbool = false;
+            bool engSmallLetterbool = false;
+            bool symbolsbool = false;
+
+            string actual = password.Create(200, options);
+            Console.WriteLine(actual);
+
+            foreach (var item in actual)
+            {
+                if (password.rusCapitalLetterList.Contains(item) == true)
+                {
+                    rusCapitalLetterbool = true;
+                }
+                if (password.rusSmallLetterList.Contains(item) == true)
+                {
+                    rusSmallLetterbool = true;
+                }
+                if (password.engSmallLetterList.Contains(item) == true)
+                {
+                    engCapitalLetterbool = true;
+                }
+                if (password.engCapitalLetterList.Contains(item) == true)
+                {
+                    engSmallLetterbool = true;
+                }
+                if (password.symbolsList.Contains(item) == true)
+                {
+                    symbolsbool = true;
+                }
+            }
+            Assert.AreEqual(true, rusCapitalLetterbool);
+            Assert.AreEqual(true, rusSmallLetterbool);
+            Assert.AreEqual(true, engCapitalLetterbool);
+            Assert.AreEqual(true, engSmallLetterbool);
+            Assert.AreEqual(true, symbolsbool);
+        }
+    }
+}
